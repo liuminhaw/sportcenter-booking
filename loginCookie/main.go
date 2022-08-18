4b183f51f370a7e734a00a68384c0ea562ad0bbf
Min-Haw, Liu <liuminhaw@gmail.com>
2022-08-18 11:31:34 +0800

    feat: automation login and retrieve session cookie
    - Read captcha answer using aws rekognition service
    - Automation login with chromedp
    - Retrieve and store user's session cookie

--- loginCookie/main.go
+++ loginCookie/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -14,7 +14,10 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/rekognition"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/chromedp/cdproto/network"
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
 	"github.com/liuminhaw/sportcenter-booking/registry"
@@ -55,31 +58,28 @@ func main() {
 		log.Fatalf("error when unmarhsal from secret manager: %v\n", err.Error())
 	}
 
-	svc := s3.New(sess)
-	input := &s3.ListObjectsV2Input{
-		Bucket:  aws.String(os.Getenv("_S3_BUCKET")),
-		MaxKeys: aws.Int64(10),
-		Prefix:  aws.String("registry"),
-	}
-	objects, err := svc.ListObjectsV2(input)
+	cookieObjs, err := getS3Objects(sess, os.Getenv("_S3_BUCKET"), "cookies/")
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case s3.ErrCodeNoSuchBucket:
-				fmt.Println(s3.ErrCodeNoSuchBucket, aerr.Error())
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
-		}
-		return
+		panic(err)
+	}
+	registryObjs, err := getS3Objects(sess, os.Getenv("_S3_BUCKET"), "registry/")
+	if err != nil {
+		panic(err)
 	}
 
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", *headless),
+	)
+
+	var cookies = map[string]bool{}
+	for _, obj := range cookieObjs.Contents {
+		fmt.Printf("cookie key: %s\n", filepath.Base(*obj.Key))
+		cookies[filepath.Base(*obj.Key)] = true
+	}
+	log.Printf("Initial cookies: %+v", cookies)
+
 	var reserveInfo registry.Reservation
-	for _, obj := range objects.Contents {
+	for _, obj := range registryObjs.Contents {
 		s3Registry := registry.Registry{
 			Bucket:   os.Getenv("_S3_BUCKET"),
 			Dirname:  filepath.Dir(*obj.Key),
@@ -93,12 +93,50 @@ func main() {
 			log.Fatalf("error when unmarhsal from registry: %v\n", err.Error())
 		}
 		fmt.Printf("Reserve information: %+v\n", reserveInfo)
+
+		if cookies[reserveInfo.Username] {
+			log.Printf("Cookie for %s exist", reserveInfo.Username)
+		} else {
+			genCookie(opts, sess, reserveInfo)
+			cookies[reserveInfo.Username] = true
+		}
 	}
 
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", *headless),
-	)
+	time.Sleep(time.Second * 3)
 
+	// fmt.Printf("Title: %s\n", title)
+}
+
+// getS3Objects returns a slice of *s3.ListObjectsV2Output from s3://bucket/prefix/,
+// and only returns objects without prefix/
+func getS3Objects(sess *session.Session, bucket string, prefix string) (*s3.ListObjectsV2Output, error) {
+	svc := s3.New(sess)
+
+	objects, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket:     aws.String(bucket),
+		Prefix:     aws.String(prefix),
+		StartAfter: aws.String(prefix),
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case s3.ErrCodeNoSuchBucket:
+				fmt.Println(s3.ErrCodeNoSuchBucket, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return nil, err
+	}
+
+	return objects, nil
+}
+
+func genCookie(opts []func(*chromedp.ExecAllocator), sess *session.Session, reserveInfo registry.Reservation) {
 	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
 	defer cancel()
 	ctx, cancel = chromedp.NewContext(ctx)
@@ -106,7 +144,6 @@ func main() {
 
 	chromedp.ListenTarget(ctx, func(ev interface{}) {
 		if _, ok := ev.(*page.EventJavascriptDialogOpening); ok {
-			// fmt.Println("closing alert:", ev.Message)
 			go func() {
 				if err := chromedp.Run(ctx,
 					page.HandleJavaScriptDialog(true),
@@ -117,20 +154,78 @@ func main() {
 		}
 	})
 
-	var title string
 	var imgBuf []byte
 	if err := chromedp.Run(ctx,
 		chromedp.Navigate(os.Getenv("_DAAN_LOGIN")),
+		chromedp.WaitVisible("#ContentPlaceHolder1_loginid", chromedp.ByID),
+		chromedp.WaitVisible("#loginpw", chromedp.ByID),
+		chromedp.WaitVisible("#ContentPlaceHolder1_Captcha_text", chromedp.ByID),
+		chromedp.WaitVisible("#ContentPlaceHolder1_CaptchaImage", chromedp.ByID),
+		chromedp.WaitVisible("#login_but", chromedp.ByID),
+		chromedp.SendKeys("#ContentPlaceHolder1_loginid", reserveInfo.Username, chromedp.ByID),
+		chromedp.SendKeys("#loginpw", reserveInfo.Password, chromedp.ByID),
 		chromedp.Screenshot("#ContentPlaceHolder1_CaptchaImage", &imgBuf, chromedp.NodeVisible),
-		chromedp.Title(&title),
 	); err != nil {
 		log.Fatal(err)
 	}
 
-	if err := ioutil.WriteFile("/tmp/captcha.png", imgBuf, 0o644); err != nil {
+	reader := bytes.NewReader(imgBuf)
+	uploader := s3manager.NewUploader(sess)
+	uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(os.Getenv("_S3_BUCKET")),
+		Key:    aws.String(fmt.Sprintf("captcha/%s.png", reserveInfo.Username)),
+		Body:   reader,
+	})
+
+	rekognitionSvc := rekognition.New(sess)
+	resp, err := rekognitionSvc.DetectText(&rekognition.DetectTextInput{
+		Image: &rekognition.Image{
+			S3Object: &rekognition.S3Object{
+				Bucket: aws.String(os.Getenv("_S3_BUCKET")),
+				Name:   aws.String(fmt.Sprintf("captcha/%s.png", reserveInfo.Username)),
+			},
+		},
+	})
+	var captchText string
+	if err == nil {
+		for _, info := range resp.TextDetections {
+			if *info.Type == "LINE" {
+				fmt.Printf("Confidence: %f\n", *info.Confidence)
+				fmt.Printf("Text: %s\n", *info.DetectedText)
+				captchText = *info.DetectedText
+				break
+			}
+		}
+	}
+
+	if err := chromedp.Run(ctx,
+		chromedp.SendKeys("ContentPlaceHolder1_Captcha_text", captchText, chromedp.ByID),
+		chromedp.Click("#login_but", chromedp.NodeVisible),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			cookies, err := network.GetAllCookies().Do(ctx)
+			if err != nil {
+				return err
+			}
+			for i, c := range cookies {
+				log.Printf("chrome cookie %d: %+v - %+v", i, c.Name, c.Value)
+				if c.Name == "ASP.NET_SessionId" {
+					cookie, err := c.MarshalJSON()
+					if err != nil {
+						log.Fatal(err)
+					}
+					fmt.Printf("Marshal cookie: %s\n", cookie)
+					reader := bytes.NewReader(cookie)
+					uploader := s3manager.NewUploader(sess)
+					uploader.Upload(&s3manager.UploadInput{
+						Bucket: aws.String(os.Getenv("_S3_BUCKET")),
+						Key:    aws.String(fmt.Sprintf("cookies/%s", reserveInfo.Username)),
+						Body:   reader,
+					})
+				}
+			}
+			return nil
+		}),
+	); err != nil {
 		log.Fatal(err)
 	}
-	time.Sleep(time.Second * 3)
-
-	// fmt.Printf("Title: %s\n", title)
 }
